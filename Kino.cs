c9a8a20f917550f495d8a8c302e87e150fdc37b9
opilane <opilane@HP-CZC2349HTC>
2024-11-28 10:00:47 +0200

    Add project files.

---
+++ Kino.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktiline_too_Kino_andmebaasiga
+{
+    public partial class Kino : Form
+    {
+        Button btn1, btn2;
+        public Kino()
+        {
+            this.Height = 676; 
+            this.Width = 881;
+            this.Text = "Tere tulemast kinno!";
+            // Устанавливаем картинку в качестве фона
+            this.BackgroundImage = Image.FromFile(@"../../Taustal.jpg");
+            // Устанавливаем стиль растяжения изображения
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            //btn1 - Kava
+            btn1 = new Button();
+            btn1.Text = "Kava";
+            btn1.Size = new Size(152, 55);
+            btn1.Location = new Point(58, 84);
+            btn1.Font= new Font("Algerian", 18, FontStyle.Italic);
+            Controls.Add(btn1);
+
+            //btn2 - Osta pilet
+            btn2 = new Button();
+            btn2.Text = "Osta pilet";
+            btn2.Size = new Size(152,55);
+            btn2.Location = new Point(58, 214);
+            btn2.Font = new Font("Algerian", 18, FontStyle.Italic);
+            Controls.Add(btn2);
+        }
+    }
+}
---
+++ Registreerimine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Praktiline_too_Kino_andmebaasiga
+{
+    public partial class Registreerimine : Form
+    {
+        Button btn1, btn2;
+        Label lbl1, lbl2, lbl3;
+        public Registreerimine()
+        {
+            this.Height = 500; 
+            this.Width = 600;
+            this.Text = "Registreerimine";
+            // Устанавливаем картинку в качестве фона
+            this.BackgroundImage = Image.FromFile(@"../../Registr.jpg");
+            // Устанавливаем стиль растяжения изображения
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            //btn1 - Kava
+            btn1 = new Button();
+            btn1.Text = "Administraator";
+            btn1.Size = new Size(230, 52);
+            btn1.Location = new Point(26,339);
+            btn1.Font = new Font("Algerian", 18, FontStyle.Italic);
+            Controls.Add(btn1);
+
+            //btn2 - Osta pilet
+            btn2 = new Button();
+            btn2.Text = "Logi sisse";
+            btn2.Size = new Size(171, 52);
+            btn2.Location = new Point(300, 339);
+            btn2.Font = new Font("Algerian", 18, FontStyle.Italic);
+            Controls.Add(btn2);
+
+            lbl1 = new Label();
+            lbl1.AutoSize = true;
+            lbl1.Text = "Nimi";
+            lbl1.Font = new Font("Arial", 25, FontStyle.Italic);
+            lbl1.TextAlign = ContentAlignment.TopLeft;
+            lbl1.Location = new Point(97, 78);
+            lbl1.Size = new Size(70, 30);
+            Controls.Add(lbl1);
+
+            lbl2 = new Label();
+            lbl3 = new Label();
+        }
+    }
+}
